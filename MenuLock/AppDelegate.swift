414421212d489120bf80c8e98ba67f05cfa3e532
Francois 'ftiff' Levaux-Tiffreau <fti@me.com>
2016-02-29 15:45:36 +0100

    Initial Commit

---
+++ MenuLock/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuLock
+//
+//  Created by François Levaux on 29.02.16.
+//  Copyright © 2016 François Levaux. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
